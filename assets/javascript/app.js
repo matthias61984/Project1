ae1523cf96f817517eb1bf8825dc031aae967d61
Doctor Mew <trc379@gmail.com>
2018-09-29 13:53:25 -0700

    Added google maps api and directions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -80,3 +80,111 @@ $("#itemDisplay").empty();
 
 
  //End of Youtoube API
+
+ /////              <-- google maps API call portion -->               /////
+
+
+      // hardcoded starting address (eventually this address will be replaced by users input)
+      var str = '3734 Merrimac Ave San Diego CA 92117'
+      var homeLat = "";
+      var homeLong = "";
+
+    // ajax call to return the lat and long of the code above
+      $.ajax({
+        url: "https://maps.googleapis.com/maps/api/geocode/json?address="+str.replace(/ /g , "+")+"&key=AIzaSyBuZheeR3N1k1UietgzIOlPCFi53bd8Xcw",
+        method: "GET"
+      }).then (function (response){
+        homeLat = result.geometry.location.lat
+        homeLong = result.geometry.location.lng
+        console.log(response);
+      })
+
+
+
+      // hard coded beginning and ending of the route 
+var start = '3734 Merrimac Ave, San Diego, CA 92117'
+var end = '8990 Miramar Rd #140, San Diego, CA 92126'
+
+      function initMap() {
+      var markerArray = [];
+      // Instantiate a directions service.
+      var directionsService = new google.maps.DirectionsService;
+
+      // Create a map and center it on Manhattan.
+      var map = new google.maps.Map(document.getElementById('itemDisplay'), {
+        zoom: 20,
+        center: {lat: homeLat, lng: homeLong}
+      });
+
+      // Create a renderer for directions and bind it to the map.
+      var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+
+      // Instantiate an info window to hold step text.
+      var stepDisplay = new google.maps.InfoWindow;
+
+      // Display the route between the initial start and end selections.
+      calculateAndDisplayRoute(
+          directionsDisplay, directionsService, markerArray, stepDisplay, map);
+      // Listen to change events from the start and end lists.
+      var onChangeHandler = function() {
+        calculateAndDisplayRoute(
+            directionsDisplay, directionsService, markerArray, stepDisplay, map);
+      };
+      // document.getElementById('start').addEventListener('change', onChangeHandler);
+      // document.getElementById('end').addEventListener('change', onChangeHandler);
+    }
+
+    function calculateAndDisplayRoute(directionsDisplay, directionsService,
+        markerArray, stepDisplay, map) {
+      // First, remove any existing markers from the map.
+      for (var i = 0; i < markerArray.length; i++) {
+        markerArray[i].setMap(null);
+      }
+
+      // Retrieve the start and end locations and create a DirectionsRequest using
+      // WALKING directions.
+      directionsService.route({
+        origin: start, //document.getElementById('start').value,
+        destination: end, //document.getElementById('end').value,
+        travelMode: 'DRIVING'
+      }, function(response, status) {
+        // Route the directions and pass the response to a function to create
+        // markers for each step.
+        if (status === 'OK') {
+          // document.getElementById('warnings-panel').innerHTML =
+          //     '<b>' + response.routes[0].warnings + '</b>';
+          directionsDisplay.setDirections(response);
+          showSteps(response, markerArray, stepDisplay, map);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
+
+    function showSteps(directionResult, markerArray, stepDisplay, map) {
+      // For each step, place a marker, and add the text to the marker's infowindow.
+      // Also attach the marker to an array so we can keep track of it and remove it
+      // when calculating new routes.
+      var myRoute = directionResult.routes[0].legs[0];
+      for (var i = 0; i < myRoute.steps.length; i++) {
+        var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+        marker.setMap(map);
+        marker.setPosition(myRoute.steps[i].start_location);
+        attachInstructionText(
+            stepDisplay, marker, myRoute.steps[i].instructions, map);
+        var directionText = myRoute.steps[i].instructions;
+        var directionDiv = $("<div>");
+        var stepNumber = i + 1 + ".";
+        directionDiv.html(stepNumber + directionText);
+        $("#directionDisplay").append(directionDiv);
+      }
+    }
+
+    function attachInstructionText(stepDisplay, marker, text, map) {
+      google.maps.event.addListener(marker, 'click', function() {
+        // Open an info window when the marker is clicked on, containing the text
+        // of the step.
+        stepDisplay.setContent(text);
+        stepDisplay.open(map, marker);
+      });
+    }
